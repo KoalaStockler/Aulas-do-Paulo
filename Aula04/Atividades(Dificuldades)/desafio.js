d1c8ad5ae3a5b8f5629079ae8687aaec71b8a6b7
Koala <koalakeno@gmail.com>
2024-06-21 13:25:54 -0300

    Simplificação do código

--- Aula04/Atividades(Dificuldades)/desafio.js
+++ Aula04/Atividades(Dificuldades)/desafio.js
@@ -3,15 +3,15 @@ let a = 1
 let b = -5
 let c = 6
 
-let baskhara = b * b - 4 * a * c;
+let delta = b * b - 4 * a * c
 
-if (baskhara > 0) {
-    let x1 = (-b + Math.sqrt(baskhara)) / (2 * a);
-    let x2 = (-b - Math.sqrt(baskhara)) / (2 * a);
-    console.log(`As raízes da equação são x1 = ${x1} e x2 = ${x2}`);
-} else if (baskhara === 0) {
-    let x = -b / (2 * a);
-    console.log(`A equação possui uma raiz dupla em x = ${x}`);
+if (delta > 0) {
+    let x1 = (-b + Math.sqrt(delta)) / (2 * a)
+    let x2 = (-b - Math.sqrt(delta)) / (2 * a)
+    console.log("As raízes da equação são x1 =", x1, "e x2 =", x2)
+} else if (delta == 0) {
+    let x = -b / (2 * a)
+    console.log("A equação possui uma raiz dupla em x =", x)
 } else {
-    console.log('A equação não possui raízes reais.');
+    console.log('A equação não possui raízes reais.')
 }
